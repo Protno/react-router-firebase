ef052574fd94fb743a705a3664e03f934e36164c
Protno <protno12@gmail.com>
2022-04-11 00:19:09 +0600

    firebase googlesignin added

---
+++ src/component/Register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register Now</h3>
+            <form action="">
+                <input type="email" placeholder='Your Name' />
+                <br/>
+                <input type="email" placeholder='Your Email' />
+                <br/>
+                <input type="password" name="" id="" placeholder='Inter Password' />
+                <br/>
+                <input type="submit" value="Register"/>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
